240c209c922973b80bf241bda4419ee78038856d
fabiodelllima <f.f.fabiodelima@gmail.com>
2026-02-27 16:39:52 -0300

    chore(api): Initial project scaffolding

---
+++ apps/api/src/main/java/com/ando/ibms/IbmsApiApplication.java
@@ -0,0 +1,12 @@
+package com.ando.ibms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmsApiApplication.class, args);
+	}
+}
