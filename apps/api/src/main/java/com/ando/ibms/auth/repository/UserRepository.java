05d4c04727dcff7224c406c048438cf96dfeb666
fabiodelllima <f.f.fabiodelima@gmail.com>
2026-03-12 12:34:28 -0300

    feat(auth): Add UserRepository

---
+++ apps/api/src/main/java/com/ando/ibms/auth/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ando.ibms.auth.repository;
+
+import com.ando.ibms.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
